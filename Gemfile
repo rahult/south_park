cb294d03240d79a9dd931418333705bd36475e0f
Rahul Trikha <rahul.trikha@gmail.com>
2012-08-07 15:09:39 +0930

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.7'
+gem 'sqlite3'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+gem 'random_record'
+
+group :development do
+  gem 'pry-rails'
+  gem 'pry-doc'
+  gem 'pry-theme'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SouthPark::Application.config.secret_token = 'a83cd852c4d5b2dbd384c0235fa9121838c6513ecace69ac1c6410d0e78469cda34c410e1f343566328a3bf36e1a917a44cd55e75fb157ea3cc08f1d2d565148'
